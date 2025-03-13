f11fb345cb317fd589a89be431f3ef5bc1cad259
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-13 17:27:06 +0800

    react路由基础版

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 const router = createBrowserRouter([
-  {path:'/',element:<div>Home</div>},
+  {
+    path: '/login',
+    element: <div>登录页</div>
+  },
+  {
+    path: '/article',
+    element: <div>文章页</div>
+  },
 ])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
     {/* <App /> */}
   </StrictMode>,
 )
