a63574dd745fa7da699cf776cd81898919b35200
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-14 13:33:35 +0800

    @/解析为src/

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,7 @@
 import Login from '../page/login/index.jsx'
 import Article from '../page/article/index.jsx'
 import LoginSon from '../page/login/loginson/loginson.jsx'
+import NotFound from '../page/notfound/index.jsx'
 import { createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
@@ -20,5 +21,9 @@ const router = createBrowserRouter([
         path: '/article',
         element: <Article />
     },
+    {
+        path: '*',
+        element: <NotFound />
+    }
 ])
 export default router;
