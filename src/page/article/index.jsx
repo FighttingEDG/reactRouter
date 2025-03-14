a56617d99b7c0b71f701d94dddab33f8574ad3f8
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-14 09:31:03 +0800

    编程式导航

--- src/page/article/index.jsx
+++ src/page/article/index.jsx
@@ -1,6 +1,10 @@
 // 单独文件组件
 // 其实就是一个函数返回html及逻辑，最后抛出
+import { Link } from "react-router-dom";
 const Article = () => {
-    return <div>文章组件</div>
+    return <div>
+        文章组件
+        <Link to="/login">登录</Link>
+    </div>
 }
 export default Article;
--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom";
 const Login = () => {
-    return <div>登陆</div>
+    return <div>
+        登陆
+        <Link to="/article">文章</Link>
+    </div>
 }
 export default Login;
