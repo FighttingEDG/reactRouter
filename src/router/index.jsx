1c424c020625571090d233633f6b9a148c6abc3a
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-14 10:04:26 +0800

    默认显示二级路由内容

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
         element: <Login />,
         children:[
             {
-                path: 'loginson',
+                index: true,
                 element: <LoginSon />
             }
         ]
